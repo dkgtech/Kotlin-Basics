10812dba7405c7ce91b796c7e48febb894a45546
dipeshguptadeveloper <dipeshguptadeveloper@gmail.com>
2023-12-19 22:26:02 +0530

    WhileLoop.kt created

---
+++ app/src/main/java/com/dkgtech/kotlinbasics/WhileLoop.kt
@@ -0,0 +1,19 @@
+package com.dkgtech.kotlinbasics
+
+fun main() {
+    var count = 1
+
+//    while loop
+
+    while (count <= 5) {
+        println("Hello")
+        count++
+    }
+
+//    do while
+
+    do {
+        println("Happy")
+        count++
+    } while (count <= 5)
+}
