7495bc38f0e6908e7c537159b800424a817d6bad
dipeshguptadeveloper <dipeshguptadeveloper@gmail.com>
2023-12-19 22:34:55 +0530

    WhileLoop.kt created

--- app/src/main/java/com/dkgtech/kotlinbasics/WhileLoop.kt
+++ app/src/main/java/com/dkgtech/kotlinbasics/WhileLoop.kt
@@ -1,19 +1,28 @@
 package com.dkgtech.kotlinbasics
 
 fun main() {
-    var count = 1
+    val count = 5
+    var index = 1
 
-//    while loop
-
-    while (count <= 5) {
-        println("Hello")
-        count++
+    while (index <= 10) {
+//        println(count * index)
+        println("$count * $index = ${count * index}")
+        index++
     }
 
-//    do while
+    /*//    while loop
 
-    do {
-        println("Happy")
-        count++
-    } while (count <= 5)
+        while (count <= 5) {
+            println("Hello")
+            count++
+        }
+
+    //    do while
+
+        do {
+            println("Happy")
+            count++
+        } while (count <= 5)*/
+
+
 }
